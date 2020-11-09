c0835eb352aae49599b20296db5eb4b79e7fc65b
pimpek32 <37272388+pimpek32@users.noreply.github.com>
2020-11-09 16:40:54 +0100

    aniations
    added animations

--- code/PlayerCombat.cs
+++ code/PlayerCombat.cs
@@ -15,12 +15,20 @@ public class PlayerCombat : Spatial
 	[Export]
 	public float CoolDown;
 	private float cdown;
+	private bool rand = true;
+	
+	private AnimationTree animationTree;
+	private AnimationPlayer animator;
+	private AnimationNodeStateMachinePlayback anim;
 	public override void _Ready()
 	{
 		parentObj = GetParent<PlayerMovement>();
 		GD.Print(parentObj);
 		dir = parentObj._vel;
 		hit = GetParent().GetNode<RayCast>("mesh/hit");
+		animationTree = GetParent().GetNode<AnimationTree>("mesh/pim/anim");
+		animator = GetParent().GetNode<AnimationPlayer>("mesh/pim/animator");
+		anim = animationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
 		cdown = CoolDown;
 		
 	}
@@ -29,9 +37,19 @@ public class PlayerCombat : Spatial
 	{
 		if(Input.IsActionJustReleased("combat_melee") && CoolDown < 0)
 		{
+
 		if(hit.IsColliding())
+		{
+
 		GD.Print(hit.GetCollider() + " in position: " + hit.GetCollisionPoint() );
+		}
 		CoolDown = cdown;
+		rand =! rand;
+
+			if(rand)
+				anim.Start("Punch");
+			else
+				anim.Start("Punch2");
 		}
 		CoolDown-=delta;
 	}
